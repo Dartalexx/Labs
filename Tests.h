1fe371bafd2df1e8701ee35919a6bd84cd050a92
Dartalexx <71958246+Dartalexx@users.noreply.github.com>
2020-11-04 13:03:23 +0700

    Add files via upload

---
+++ Tests.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Complex.h";
+bool testMulti(Complex a, Complex b, Complex r);
+bool testDiv(Complex a, Complex b, Complex r);
+bool testEqual(Complex a, Complex b);
+bool testLSign(Complex a, Complex b);
+bool testMSign(Complex a, Complex b);
