a2d78f8b5380f3ab48ebd59948642951ca7847b6
Dartalexx <71958246+Dartalexx@users.noreply.github.com>
2020-12-21 19:17:44 +0700

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,14 +1,5 @@
 #pragma once
 #include "Complex.h";
-/*bool testMulti(Complex a, Complex b, Complex r);
-bool testDiv(Complex a, Complex b, Complex r);
-bool testEqual(Complex a, Complex b);
-bool testLSign(Complex a, Complex b);
-bool testMSign(Complex a, Complex b);
-void testConsole(Complex& a);
-bool testText(Complex& a,Complex& b);
-bool testBinary(Complex& a, Complex& b);
-*/
 void testFile();
 void testZero();
 void testString();
